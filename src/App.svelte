14fbba0e1295fd0ee7ae8bad0805b38deb6d6fca
Ayomatoi <ayomatoi@gmail.com>
2021-10-19 19:31:28 +0700

    Init my web

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script> 
+	let name = 'instagram';
+</script>
+<main>
+	<h1>ทำความรู้จักกับผมเล็กน้อย</h1>
+	<ul>
+		<li>ชื่อจริง:ธิติภาส นามสกุล:ตั้งวิญญู ชื่อเล่น:เกื้อ</li>
+		<li>อายุ:16ปี</li>
+		<li>อาชีพ:นักเรียน</li>
+		<li>โรงเรียน:ไตรพัฒน์</li>
+		<li>สิ่งที่ชอบ:เกม อนิเมะ</li>
+	</ul>
+	
+	<img src="i\5.jpg" width="500" >
+	<p>ผมเป็นคนที่ชอบเกมและอนิเมะค่อนข้างมาก ผมเลยมักจะชอบทำอะไรเกี่ยวกับพวกนี้อยู่บ่อยๆ ซึ่งสิ่งที่ผมทำนั้นคือการวาดรูปนั่นองครับ</p>
+	<a href="https://wiki.anime-os.com/">Anime</a><br>
+	<a href="https://th.wikipedia.org/wiki/%E0%B8%A7%E0%B8%B4%E0%B8%94%E0%B8%B5%E0%B9%82%E0%B8%AD%E0%B9%80%E0%B8%81%E0%B8%A1">Game</a>
+	<p>IGของผม</p>
+	<a href="https://instagram.com/ayomatoi06?utm_medium=copy_link"><button>{name}</button></a>
+	<h2>ผลงานของผม</h2>
+	<p>ผลงานส่วนใหญ่ของผมจะเป็นศิลปะครับ</p>
+	<img src="i\1.jpeg" width="500">
+	<img src="i\2.jpg" width="500">
+	<img src="i\3.jpg" width="500">
+	<img src="i\4.jpg" width="500">
+</main>
+<style>
+	h1{
+		color: rgb(5, 8, 43);
+		text-align: center;
+		font-size: 3em;
+	}
+	h2{
+		color: rgb(54, 0, 0);
+		text-align: center;
+		font-size: 3em;
+	}
+	p{
+		font-size: 6mm;
+	}
+	ul{
+		font-size: 6mm;
+	}
+	a{
+		font-size: 6mm;
+		color: rgb(255, 0, 0);
+	}
+</style>
+<a href="https://www.google.co.th/?hl=th&safe=active&ssui=on">>>Google</a><br>
